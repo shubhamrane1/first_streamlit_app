4524f089a896bf4800e539f6199e884b8af8fed7
shubhamrane1 <134258605+shubhamrane1@users.noreply.github.com>
2023-05-22 11:47:18 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('hello everyone')
+streamlit.header('brekfast menu')
+streamlit.text('eggs')
+streamlit.text('non-veg')
+streamlit.text('veg')
+streamlit.text('juice')
+streamlit.text('drinks')
